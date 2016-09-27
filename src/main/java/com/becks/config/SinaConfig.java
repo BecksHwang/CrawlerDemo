7f768bb01ef7526cf7b05735506dacd37ca68c21
BecksHwang <beckshwang@gmail.com>
2016-09-27 17:17:52 +0800

    update SinaConfig

--- src/main/java/com/becks/config/SinaConfig.java
+++ src/main/java/com/becks/config/SinaConfig.java
@@ -3,7 +3,7 @@ package com.becks.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-@Component("")
+@Component("sinaconfig")
 public class SinaConfig {
 	
 	@Value("${sinaUrl}")
